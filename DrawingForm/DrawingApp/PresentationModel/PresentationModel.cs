074106c64d504571c16301cb01d1497480b1efcf
IwasKoro <youcry987951@gmail.com>
2022-01-04 13:13:47 +0800

    mix up all

---
+++ DrawingForm/DrawingApp/PresentationModel/PresentationModel.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using DrawingModel;
+
+namespace DrawingApp.PresentationModel
+{
+    public class PresentationModel
+    {
+        public event ModelChangedEventHandler _presentationModelChanged;
+        public delegate void ModelChangedEventHandler();
+
+        readonly Model _model;
+        readonly IGraphics _iGraphics;
+
+        public PresentationModel(Model model, Canvas canvas)
+        {
+            this._model = model;
+            _iGraphics = new WindowsStoreGraphicsAdaptor(canvas);
+            _model._modelChanged += NotifyObserver;
+        }
+
+        //畫圖
+        public void Draw()
+        {
+            // 重複使用igraphics物件
+            _model.Draw(_iGraphics);
+        }
+
+        public bool LineButtonStatus
+        {
+            get; set;
+        }
+
+        public bool RectangleButtonStatus
+        {
+            get; set;
+        }
+
+        public bool EllipseButtonStatus
+        {
+            get; set;
+        }
+
+        public bool ClearButtonStatus
+        {
+            get
+            {
+                return _model.GetShapes().Count != 0;
+            }
+        }
+
+        public bool UndoButtonStatus
+        {
+            get
+            {
+                return _model.UndoStatus;
+            }
+        }
+
+        public bool RedoButtonStatus
+        {
+            get
+            {
+                return _model.RedoStatus;
+            }
+        }
+
+        //設定按鈕狀態
+        public void SetButtonStatus(bool line, bool rectangle, bool ellipse)
+        {
+            LineButtonStatus = line;
+            RectangleButtonStatus = rectangle;
+            EllipseButtonStatus = ellipse;
+        }
+
+        //改變畫圖模式
+        public int DrawingMode
+        {
+            get
+            {
+                return _model.DrawingMode;
+            }
+            set
+            {
+                _model.DrawingMode = value;
+            }
+        }
+
+        //觀察者
+        public void NotifyObserver()
+        {
+            if (_presentationModelChanged != null)
+            {
+                _presentationModelChanged();
+            }
+        }
+
+        //取得shape 的字串
+        public string GetShapeData(double xCoordinate, double yCoordinate)
+        {
+            const int DEFAULT_MODE = -1;
+            if (DrawingMode == DEFAULT_MODE)
+            {
+                IShape shape = _model.GetOnShape(xCoordinate, yCoordinate);
+                _model.MarkShape(xCoordinate, yCoordinate);
+                if (shape != null)
+                {
+                    return GetShapeName(shape) + shape.GetDataString();
+                }
+            }
+            return "";
+        }
+
+        //取得shape的名稱
+        private string GetShapeName(IShape shape)
+        {
+            const string LINE = "Line:";
+            const string RECTANGLE = "Rectangle:";
+            const string ELLIPSE = "Ellipse:";
+            if (shape.GetType() == new Line().GetType())
+                return LINE;
+            else if (shape.GetType() == new Rectangle().GetType())
+                return RECTANGLE;
+            else if (shape.GetType() == new Ellipse().GetType())
+                return ELLIPSE;
+            else
+                return "";
+        }
+    }
+}
---
+++ DrawingForm/DrawingApp/PresentationModel/WindowsStoreGraphicsAdaptor.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Shapes;
+using Windows.UI.Xaml.Media;
+using DrawingModel;
+using Windows.UI.Xaml;
+
+namespace DrawingApp.PresentationModel
+{
+    class WindowsStoreGraphicsAdaptor : IGraphics
+    {
+        readonly Canvas _canvas;
+
+        public WindowsStoreGraphicsAdaptor(Canvas canvas)
+        {
+            _canvas = canvas;
+        }
+
+        //清空全部
+        public void ClearAll()
+        {
+            _canvas.Children.Clear();
+        }
+
+        //畫線
+        public void DrawLine(double x1, double y1, double x2, double y2)
+        {
+            Windows.UI.Xaml.Shapes.Line line = new Windows.UI.Xaml.Shapes.Line
+            { 
+                X1 = x1,
+                Y1 = y1,
+                X2 = x2,
+                Y2 = y2,
+                Stroke = new SolidColorBrush(Colors.Black) };
+            _canvas.Children.Add(line);
+        }
+
+        //畫方形
+        public void DrawRectangle(double x1, double y1, double x2, double y2)
+        {
+            Windows.UI.Xaml.Shapes.Rectangle rectangle = new Windows.UI.Xaml.Shapes.Rectangle
+            { 
+                Margin = GetMarginAttribute(x1, y1, x2, y2),
+                Width = x2 > x1 ? x2 - x1 : x1 - x2,
+                Height = y2 > y1 ? y2 - y1 : y1 - y2,
+                Fill = new SolidColorBrush(Colors.Yellow),
+                Stroke = new SolidColorBrush(Colors.Black) };
+            _canvas.Children.Add(rectangle);
+        }
+
+        //畫橢圓
+        public void DrawEllipse(double x1, double y1, double x2, double y2)
+        {
+            Windows.UI.Xaml.Shapes.Ellipse ellipse = new Windows.UI.Xaml.Shapes.Ellipse
+            { 
+                Margin = GetMarginAttribute(x1, y1, x2, y2),
+                Width = x2 > x1 ? x2 - x1 : x1 - x2,
+                Height = y2 > y1 ? y2 - y1 : y1 - y2,
+                Fill = new SolidColorBrush(Colors.Orange),
+                Stroke = new SolidColorBrush(Colors.Black) };
+            _canvas.Children.Add(ellipse);
+        }
+
+        //設定圖形Margin的值
+        private Thickness GetMarginAttribute(double x1, double y1, double x2, double y2)
+        {
+            double left = x2 > x1 ? x1 : x2;
+            double right = _canvas.MaxWidth - (x2 > x1 ? x2 : x1);
+            double top = y2 > y1 ? y1 : y2;
+            double bottom = _canvas.MaxHeight - (y2 > y1 ? y2 : y1);
+            return new Thickness(left, top, right, bottom);
+        }
+
+        //畫選擇的物件
+        public void DrawSelectedItem(double x1, double y1, double x2, double y2)
+        {
+            const float THICKNESS = 3;
+            const float DASH = 7;
+            Windows.UI.Xaml.Shapes.Rectangle selectedItem = new Windows.UI.Xaml.Shapes.Rectangle
+            { 
+                Margin = GetMarginAttribute(x1, y1, x2, y2),
+                Width = x2 > x1 ? x2 - x1 : x1 - x2,
+                Height = y2 > y1 ? y2 - y1 : y1 - y2,
+                Stroke = new SolidColorBrush(Colors.Red),
+                StrokeThickness = THICKNESS,
+                StrokeDashArray =
+                { 
+                    DASH, THICKNESS } };
+            _canvas.Children.Add(selectedItem);
+            DrawCorner(x1, y1, x2, y2);
+        }
+
+        //畫角落
+        private void DrawCorner(double x1, double y1, double x2, double y2)
+        {
+            const float HALF_RADIUS = 4;
+            DrawPoint(x1 - HALF_RADIUS, y1 - HALF_RADIUS);
+            DrawPoint(x1 - HALF_RADIUS, y2 - HALF_RADIUS);
+            DrawPoint(x2 - HALF_RADIUS, y1 - HALF_RADIUS);
+            DrawPoint(x2 - HALF_RADIUS, y2 - HALF_RADIUS);
+        }
+
+        //畫角落的點
+        private void DrawPoint(double xCoordinate, double yCoordinate)
+        {
+            const float RADIUS = 8;
+            Windows.UI.Xaml.Shapes.Ellipse point = new Windows.UI.Xaml.Shapes.Ellipse
+            { 
+                Margin = GetMarginAttribute(xCoordinate, yCoordinate, xCoordinate + RADIUS, yCoordinate + RADIUS),
+                Width = RADIUS,
+                Height = RADIUS,
+                Fill = new SolidColorBrush(Colors.White),
+                Stroke = new SolidColorBrush(Colors.Black),
+                StrokeThickness = 1 };
+            _canvas.Children.Add(point);
+        }
+    }
+}
